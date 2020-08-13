6c42f62db2176f5cfe53a9977aae32c398adf0fd
Alexandro T. Netto <alex.t.netto@gmail.com>
2020-08-13 16:47:23 -0300

    List teachers available based on query

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,26 +4,35 @@ import whatsLogo from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-function TeacherItem() {
+interface TeacherItemProps {
+  teacher: {
+    name: string;
+    avatar: string;
+    subject: string;
+    bio: string;
+    cost: number;
+    whatsapp: string;
+  }
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return(
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/56097505?s=460&u=287ff8cc98b046f4ecaa578b5e0ce95d803e3ebe&v=4" alt="Alexadro T. Netto"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Alexandro T. Netto</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
       <p>
-        Descrição e 
-        <br/><br/>
-        talz
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$50.00</strong>
+          <strong>R${teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsLogo} alt="Whatsapp"/>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Index';
 import Select from '../../components/Select';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherList (){
+function TeacherList() {
+	const [state, setState] = useState({
+		subject: '',
+		week_day: '',
+		time: ''
+	})
+
+	const [stateTeacherList, setTeacherList] = useState([])
+
+	async function searchTeachers(e: FormEvent) {
+		const response = await api.get('classes', {
+			params: {
+				...state
+			},
+		})
+		setTeacherList(response.data)
+		console.log(response.data)
+	}
+
 	return (
 		<div id="page-teacher-list" className="container">
 			<PageHeader title="Estes são os proffys disponíveis">
-				<form id="search-teachers">
+				<div id="search-teachers">
 				<Select
 						name="subject"
 						label="Matéria"
+						onChange={(e) => {
+							setState({...state, subject: e.target.value})
+						}}
+						value={state.subject}
 						options={[
 							{ value:'Artes', label:'Artes'},
 							{ value:'Biologia', label:'Biologia'},
@@ -29,8 +53,12 @@ function TeacherList (){
 						]}
 					/>
 					<Select
-						name="week_da7"
+						name="week_day"
 						label="Dia da semana"
+						onChange={(e) => {
+							setState({...state, week_day: e.target.value})
+						}}
+						value={state.week_day}
 						options={[
 							{ value:'0', label:'Domingo'},
 							{ value:'1', label:'Segunda-feira'},
@@ -41,15 +69,25 @@ function TeacherList (){
 							{ value:'6', label:'Sábado'},
 						]}
 					/>
-					<Input name="time" label="Hora" type="time"/>
-				</form>
+					<Input
+						name="time"
+						label="Hora"
+						onChange={(e) => {
+							setState({...state, time: e.target.value})
+						}}
+						value={state.time}
+						type="time"
+					/>
+					<button type="button" onClick={searchTeachers}>
+						Buscar
+					</button>
+				</div>
 			</PageHeader>
 
 			<main>
-			<TeacherItem />
-			<TeacherItem />
-			<TeacherItem />
-			<TeacherItem />
+				{stateTeacherList.map((teacher, index) => {
+					return <TeacherItem key={index} teacher={teacher}  />
+				})}
 			</main>
 		</div>
   )
